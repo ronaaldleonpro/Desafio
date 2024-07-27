f493f580410cb013ce22212e858f8d00720cb718
ronaaldleonpro <ronaldleonpro@gmail.com>
2024-07-27 10:44:20 -0600

    Modificación de diseño general para una mejor vista

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -3,22 +3,28 @@ import { data } from "../data";
 
 export const ProductList = ({}) => {
   return (
-    <div class="container-items">
+    <div className="container-items">
       {data.map((product) => (
-        <div class="item" key={product.id}>
-          <div class="item-title-container">
-            <h2 class="item-title">{product.title}</h2>
+        <div className="item" key={product.id}>
+          <div className="item-image-container">
+            <img
+              src={product.imagen}
+              alt={product.title}
+              className="item-image"
+            />
           </div>
-          <div class="item-info-container">
-            <p class="item-info">{product.descripcion}</p>
-          </div>
-          <div class="item-image-container">
-            <img src={product.imagen} alt={product.title} class="item-image" />
-          </div>
-          <div class="item-specif-container">
-            <p class="item-specif">
-              CARÁCTERISTICAS: {product.caracteristicas}
-            </p>
+          <div className="item-content">
+            <div className="item-title-container">
+              <h2 className="item-title">{product.title}</h2>
+            </div>
+            <div className="item-info-container">
+              <p className="item-info">{product.descripcion}</p>
+            </div>
+            <div className="item-specif-container">
+              <p className="item-specif">
+                CARÁCTERISTICAS: {product.caracteristicas}
+              </p>
+            </div>
           </div>
         </div>
       ))}
